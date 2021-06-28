392afadc7e51fed633d002976973ab5c9b65b3e5
Vinayak-Mahadev <vinay.nagaraj@enhancesys.com>
2021-06-28 16:38:13 +0530

    Update --Working version

--- datagrokr-api/datagrokr-services/src/main/java/com/datagrokr/integration/services/conf/CORSFilter.java
+++ datagrokr-api/datagrokr-services/src/main/java/com/datagrokr/integration/services/conf/CORSFilter.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
@@ -21,23 +22,22 @@ import org.springframework.stereotype.Component;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CORSFilter  implements Filter {
 
+	@Value("${spring.cors.origins}")
+	private String allowOrigin;
+	
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException 
 	{
 		HttpServletResponse response = (HttpServletResponse) res;
 		HttpServletRequest request = (HttpServletRequest) req;
-		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Origin", allowOrigin);
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, POST, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
 		if(request.getMethod().equals(HttpMethod.OPTIONS.name()))
-		{
 			response.setStatus(HttpStatus.NO_CONTENT.value());
-		}
 		else
-		{
 			chain.doFilter(req, res);
-		}
 	}
 
 	public void init(FilterConfig filterConfig) {}
